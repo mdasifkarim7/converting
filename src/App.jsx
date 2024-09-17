33dd769954754909a37ee1ca6194730247010679
md ashif karim <mdasifkarim7@gmail.com>
2024-09-17 23:59:24 +0530

    react_setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+// const app = () => {
+  
+// }
+
+function App() {
+  return(<h>hello</h>)
+  
+}
+// const gg=function(){}
+// const fh = () => {
+  
+// }
+export default App;
